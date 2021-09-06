44b02dac19b691b519e3c6ba79330811d6932226
iCode_Rx <arababs2015@gmail.com>
2021-09-06 18:55:45 +0100

    server done

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const express = require("express")
+const request = require('request');
 const app = express()
 const port = 5000
 
 
 
-app.get("/", (res, req) => {
-          res.setEncoding("Hello world!!!")
+app.get("/", (req, res) => {
+          res.send("Hello world!!!")
 })
+
+
+app.get("/newEndpt", (req, res) => {
+          request("https://apitemplate21.herokuapp.com/api/posts", 
+          
+                    function (error, response, body) {
+                    
+                              if(!error && response.statusCode == 200) {
+                                        const parseBody = JSON.parse(body)
+
+                                        res.send(parseBody)
+                              }
+                    }
+          );
+
+})
+
+
 
 
 app.listen(port, () => {
